2837211215c831e826f60fc1e883e089f143a8cd
igor-shaula <igor.shaula@gmail.com>
2018-09-23 23:09:39 +0300

    moved TemplateMethod pattern to new module \\

--- app/src/main/java/igor/shaula/gof_patterns/entry_points/gof_behavioral/GofTemplateMethod.java
+++ app/src/main/java/igor/shaula/gof_patterns/entry_points/gof_behavioral/GofTemplateMethod.java
@@ -1,9 +1,9 @@
 package igor.shaula.gof_patterns.entry_points.gof_behavioral;
 
 import igor.shaula.gof_patterns.general_classes.AnswerFromPattern;
-import igor.shaula.gof_patterns.gof_behavioral.template_method.EmployeeTemplate;
-import igor.shaula.gof_patterns.gof_behavioral.template_method.TeamDeveloper;
-import igor.shaula.gof_patterns.gof_behavioral.template_method.TeamTester;
+import com.igor_shaula.patterns_in_pure_java.gof_behavioral.template_method.EmployeeTemplate;
+import com.igor_shaula.patterns_in_pure_java.gof_behavioral.template_method.TeamDeveloper;
+import com.igor_shaula.patterns_in_pure_java.gof_behavioral.template_method.TeamTester;
 
 /**
  * Define the skeleton of an algorithm in an operation, deferring some steps to subclasses.
--- patterns_core/src/main/java/com/igor_shaula/patterns_in_pure_java/gof_behavioral/template_method/TeamDeveloper.java
+++ patterns_core/src/main/java/com/igor_shaula/patterns_in_pure_java/gof_behavioral/template_method/TeamDeveloper.java
@@ -1,4 +1,4 @@
-package igor.shaula.gof_patterns.gof_behavioral.template_method;
+package com.igor_shaula.patterns_in_pure_java.gof_behavioral.template_method;
 
 /**
  * @author igor shaula \
